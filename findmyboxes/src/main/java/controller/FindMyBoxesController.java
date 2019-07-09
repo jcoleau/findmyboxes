08451dacf7679da32dcd9b2825bee46293a23cee
John Griffith <john.lawrence.griffith@gmail.com>
2019-07-09 08:12:49 -0400

    Fixed the context:component-scan in the springmvc-servlet.xml configuration file. Additionally, added a header file, footer file, and a homepage.

--- findmyboxes/src/main/java/controller/FindMyBoxesController.java
+++ findmyboxes/src/main/java/controller/FindMyBoxesController.java
@@ -2,13 +2,20 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller 
 public class FindMyBoxesController {
 
-	@RequestMapping("/greeting")
-	public String displayGreeting() {
+	@RequestMapping(path = "/", method = RequestMethod.GET)
+	public String displayHomepage() {
 		
-		return "greeting";
+		return "homepage";
+	}
+	
+	@RequestMapping(path = "/findmyboxes", method = RequestMethod.GET)
+	public String displayHomepage2() {
+		
+		return "homepage";
 	}
 }
---
+++ findmyboxes/src/main/webapp/WEB-INF/jsp/homepage.jsp
@@ -0,0 +1,7 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<c:set var="pageTitle" value="Find My Boxes"/>
+<%@include file="common/header.jsp" %>
+
+<h2>Hey there! You're pointing to the Controller correctly!</h2>
+
+<%@include file="common/footer.jsp" %>
